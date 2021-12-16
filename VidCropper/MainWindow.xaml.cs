4842c8f545c5a33469149163fb2f52565fa92e58
Konrad <kpierzynski@hotmail.com>
2021-12-16 03:40:24 +0100

    Changed layout. Added codec combobox. Added +10s, -10s, play, pause buttons, added video duration

--- VidCropper/MainWindow.xaml.cs
+++ VidCropper/MainWindow.xaml.cs
@@ -66,17 +66,18 @@ namespace VidCropper
 
         private void timer_event(object sender, EventArgs e)
         {
-            duration.Text = "Duration: " + player.Position.ToString(@"hh\:mm\:ss");
+            duration.Text = "Duration: " + player.Position.ToString(@"hh\:mm\:ss") + " / " + ((player.NaturalDuration.HasTimeSpan == true) ? player.NaturalDuration.TimeSpan.ToString(@"hh\:mm\:ss") : "00:00:00");
+
         }
 
         private void generate_click(object sender, RoutedEventArgs e)
         {
-            if( String.IsNullOrEmpty(filePath) )
+            if (String.IsNullOrEmpty(filePath))
             {
                 MessageBox.Show("Open file first!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
             }
-            if (p0 == default(Point) || p1 == default(Point) )
+            if (p0 == default(Point) || p1 == default(Point))
             {
                 MessageBox.Show("Select rectangle first!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
@@ -101,7 +102,7 @@ namespace VidCropper
             int w = (p1x - p0x) < 0 ? p0x - p1x : p1x - p0x;
             int h = (p1y - p0y) < 0 ? p0y - p1y : p1y - p0y;
 
-            FormattableString cmd = $"-i \"{filePath}\" -filter:v \"crop={w}:{h}:{x}:{y}\" -c:v libx265 \"{saveFilePath}\"";
+            FormattableString cmd = $"-i \"{filePath}\" -filter:v \"crop={w}:{h}:{x}:{y}\" -c:v {(codec.SelectedItem as ComboBoxItem).Content.ToString()} \"{saveFilePath}\"";
             command.Text = "./ffmpeg.exe " + cmd.ToString();
 
             using (Process p = new Process())
@@ -115,6 +116,26 @@ namespace VidCropper
             canvas.Children.Clear();
         }
 
+        private void plus10s_Click(object sender, RoutedEventArgs e)
+        {
+            if (player.Source != default(Uri)) player.Position += TimeSpan.FromSeconds(9);
+        }
+
+        private void pause_Click(object sender, RoutedEventArgs e)
+        {
+            if (player.Source != default(Uri)) player.Pause();
+        }
+
+        private void play_Click(object sender, RoutedEventArgs e)
+        {
+            if (player.Source != default(Uri)) player.Play();
+        }
+
+        private void minus10s_Click(object sender, RoutedEventArgs e)
+        {
+            if (player.Source != default(Uri)) player.Position -= TimeSpan.FromSeconds(9);
+        }
+
         private void file_click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog open = new OpenFileDialog();
@@ -122,6 +143,7 @@ namespace VidCropper
             {
                 filePath = open.FileName;
                 player.Source = new Uri(filePath);
+                player.Play();
             }
         }
     }
